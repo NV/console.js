cde3645d41395689ac706c9fa357a735b927b6ec
Nikita Vasilyev <me@elv1s.ru>
2009-10-24 13:56:12 +0400

    Firebug lite removed. Use Firebug lite without console.js http://wiki.github.com/NV/console.js/firebug-lite

--- console.js
+++ console.js
@@ -5,164 +5,148 @@ if (typeof console === 'undefined') {
    */
   window.console = {};
 
-  if (document.body) {
+  var post = window.opera && opera.postError || alert;
 
-    var firebug = document.createElement('script');
-    firebug.setAttribute('src', 'http://getfirebug.com/releases/lite/1.2/firebug-lite-compressed.js');
-    document.body.appendChild(firebug);
-    (function(){
-      if (window.pi && window.firebug) {
-        firebug.init();
-      } else {
-        setTimeout(arguments.callee);
+  /**
+   * Limit of objects dimensions
+   * console.dimensions_limit = 0
+   * console.log([42]) ==> [?]
+   * console.dimensions_limit = 1
+   * console.log([42]) ==> [42]
+   */
+  console.dimensions_limit = 1;
+
+  /**
+   * console.log({x:2, y:8, z:[4,3]}) ==> '{ x: 2, y: 8, z: [4, 3] }'
+   * @param Objects
+   */
+  console.log = console.info = console.warn = console.error = console.debug = console.dir = function log () {
+
+    /**
+     * source_of({x:2, y:8}) === "{'x':2, 'y':8}"
+     * @param {Object} arg
+     * @param {Number} limit dimension of objects
+     * @return {String} string representation of input
+     */
+    var source_of = function source_of (arg, limit) {
+
+      if (typeof limit == 'undefined') {
+        var limit = console.dimensions_limit;
       }
-    })();
 
-  } else {
+      var result = '';
 
-    var post = window.opera && opera.postError || alert;
-
-    /**
-     * Limit of objects dimensions
-     * console.dimensions_limit = 0
-     * console.log([42]) ==> [?]
-     * console.dimensions_limit = 1
-     * console.log([42]) ==> [42] 
-     */
-    console.dimensions_limit = 1;
-
-    /**
-     * console.log({x:2, y:8, z:[4,3]}) ==> '{ x: 2, y: 8, z: [4, 3] }'
-     * @param Objects
-     */
-    console.log = console.info = console.warn = console.error = console.debug = console.dir = function log () {
-
-      /**
-       * source_of({x:2, y:8}) === "{'x':2, 'y':8}"
-       * @param {Object} arg
-       * @param {Number} limit dimension of objects
-       * @return {String} string representation of input
-       */
-      var source_of = function source_of (arg, limit) {
-
-        if (typeof limit == 'undefined') {
-          var limit = console.dimensions_limit;
+      // Is array?
+      if (arg && typeof arg.length === 'number' && typeof arg.splice === 'function') {
+        if (!limit) return '[?]';
+        result += '[';
+        var arr_list = [];
+        for (var j = 0; j < arg.length; j++) {
+          arr_list.push( source_of(arg[j], limit-1) );
         }
-
-        var result = '';
-
-        // Is array?
-        if (arg && typeof arg.length === 'number' && typeof arg.splice === 'function') {
-          if (!limit) return '[?]';
-          result += '[';
-          var arr_list = [];
-          for (var j = 0; j < arg.length; j++) {
-            arr_list.push( source_of(arg[j], limit-1) );
-          }
-          result += arr_list.join(', ') +']';
-        } else if (typeof arg == 'object') {
-          if (!limit) return '{?}';
-          result += '{ ';
-          var arr_obj = [];
-          for (var key in arg) {
-            arr_obj.push( "'"+ key +"': "+ source_of(arg[key], limit-1) );
-          }
-          result += arr_obj.join(', ') +' }';
-        } else if (typeof arg == 'string') {
-          return "'"+ arg +"'";
-        } else {
-          return arg;
+        result += arr_list.join(', ') +']';
+      } else if (typeof arg == 'object') {
+        if (!limit) return '{?}';
+        result += '{ ';
+        var arr_obj = [];
+        for (var key in arg) {
+          arr_obj.push( "'"+ key +"': "+ source_of(arg[key], limit-1) );
         }
-
-        return result;
-      };
-
-      for (var i=0; i<arguments.length; i++) {
-        post( source_of(arguments[i]) );
-      }
-
-    };
-
-
-    /**
-     * Simplified version of http://eriwen.com/javascript/js-stack-trace/
-     */
-    console.trace = function trace() {
-      try {
-        i.dont.exist++;
-      } catch(e) {
-        var stack = e.stack || e.stacktrace;
-        stack = stack.split('\n').slice(2).join('\n');
-        post(stack);
-      }
-    };
-
-
-    /**
-     * console.assert(false, "I'm gonna fail")
-     * @param {Boolean} is_ok
-     * @param {String} message optional
-     */
-    console.assert = function assert (is_ok, message) {
-      if (!is_ok) post( 'ASSERT FAIL: '+ message );
-    };
-
-
-    /**
-     * @param {String} name optional
-     */
-    console.group = console.groupCollapsed = function group (name) {
-      post('\n-------------------- '+ name +' --------------------');
-    };
-
-
-    /**
-     * Print 3 line breaks
-     */
-    console.groupEnd = function groupEnd () {
-      post('\n\n\n');
-    };
-
-
-    /**
-     * @param {String} title optional
-     */
-    console.count = function count (title) {
-      var title = title || '';
-      arguments.callee.counters = arguments.callee.counters || {};
-      if (arguments.callee.counters[title]) {
-        arguments.callee.counters[title]++;
+        result += arr_obj.join(', ') +' }';
+      } else if (typeof arg == 'string') {
+        return "'"+ arg +"'";
       } else {
-        arguments.callee.counters[title] = 1;
+        return arg;
       }
-      post(title +' '+ arguments.callee.counters[title]);
+
+      return result;
     };
 
+    for (var i=0; i<arguments.length; i++) {
+      post( source_of(arguments[i]) );
+    }
 
-    /**
-     * @param {String} title optional
-     */
-    console.profile = function profile (title) {
-      return 'Not implemented';
-    };
-    console.profileEnd = function profileEnd () {
-      return 'Not implemented';
-    };
+  };
 
 
-    /**
-     * @param {String} name
-     */
-    console.time = function time (name) {
-      return 'Not implemented';
-    };
-    /**
-     * @param {String} name
-     */
-    console.timeEnd = function timeEnd (name) {
-      return 'Not implemented';
-    };
+  /**
+   * Simplified version of http://eriwen.com/javascript/js-stack-trace/
+   */
+  console.trace = function trace() {
+    try {
+      i.dont.exist++;
+    } catch(e) {
+      var stack = e.stack || e.stacktrace;
+      stack = stack.split('\n').slice(2).join('\n');
+      post(stack);
+    }
+  };
 
 
-  }
+  /**
+   * console.assert(false, "I'm gonna fail")
+   * @param {Boolean} is_ok
+   * @param {String} message optional
+   */
+  console.assert = function assert (is_ok, message) {
+    if (!is_ok) post( 'ASSERT FAIL: '+ message );
+  };
+
+
+  /**
+   * @param {String} name optional
+   */
+  console.group = console.groupCollapsed = function group (name) {
+    post('\n-------------------- '+ name +' --------------------');
+  };
+
+
+  /**
+   * Print 3 line breaks
+   */
+  console.groupEnd = function groupEnd () {
+    post('\n\n\n');
+  };
+
+
+  /**
+   * @param {String} title optional
+   */
+  console.count = function count (title) {
+    var title = title || '';
+    arguments.callee.counters = arguments.callee.counters || {};
+    if (arguments.callee.counters[title]) {
+      arguments.callee.counters[title]++;
+    } else {
+      arguments.callee.counters[title] = 1;
+    }
+    post(title +' '+ arguments.callee.counters[title]);
+  };
+
+
+  /**
+   * @param {String} title optional
+   */
+  console.profile = function profile (title) {
+    return 'Not implemented';
+  };
+  console.profileEnd = function profileEnd () {
+    return 'Not implemented';
+  };
+
+
+  /**
+   * @param {String} name
+   */
+  console.time = function time (name) {
+    return 'Not implemented';
+  };
+  /**
+   * @param {String} name
+   */
+  console.timeEnd = function timeEnd (name) {
+    return 'Not implemented';
+  };
+
+
 }
