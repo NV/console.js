6a1d9d9153c174abf44ba6ae85efbd971f451196
Nikita Vasilyev <me@elv1s.ru>
2009-12-24 10:07:59 +0300

    New console detection; Opera and IE logging improvements; IE console.groupEnd error fixed

--- console.js
+++ console.js
@@ -1,11 +1,13 @@
 if (typeof console === 'undefined') {
-
   /**
    * @see http://getfirebug.com/console.html
    */
   window.console = {};
+}
 
-  var post = window.opera && opera.postError || alert;
+(function(){
+  
+  var post = console.log || window.opera && opera.postError || alert;
 
   /**
    * Limit of objects dimensions
@@ -17,18 +19,17 @@ if (typeof console === 'undefined') {
   console.dimensions_limit = 1;
 
   /**
-   * console.log({x:2, y:8, z:[4,3]}) ==> '{ x: 2, y: 8, z: [4, 3] }'
-   * @param Objects
+   * source_of({x:2, y:8, z:[4,3]}) ==> '{ x: 2, y: 8, z: [4, 3] }'
    */
-  console.log = console.info = console.warn = console.error = console.debug = console.dir = function log () {
+  function source_of () {
 
     /**
-     * source_of({x:2, y:8}) === "{'x':2, 'y':8}"
+     * source_of_one_arg({x:2, y:8}) === "{'x':2, 'y':8}"
      * @param {Object} arg
      * @param {Number} limit dimension of objects
      * @return {String} string representation of input
      */
-    var source_of = function source_of (arg, limit) {
+    function source_of_one_arg (arg, limit) {
 
       if (limit === undefined) {
         var limit = console.dimensions_limit;
@@ -55,7 +56,7 @@ if (typeof console === 'undefined') {
         result = '[';
         var arr_list = [];
         for (var i=0; i<arg.length; i++) {
-          arr_list[i] = source_of(arg[i], limit);
+          arr_list[i] = source_of_one_arg(arg[i], limit);
         }
         result += arr_list.join(', ') +']';
       } else if (Object.prototype.toString.call(arg) === '[object String]') {
@@ -67,7 +68,7 @@ if (typeof console === 'undefined') {
         result = '{ ';
         var arr_obj = [];
         for (var key in arg) {
-          arr_obj.push( "'"+ key +"': "+ source_of(arg[key], limit-1) );
+          arr_obj.push( "'"+ key +"': "+ source_of_one_arg(arg[key], limit-1) );
         }
         result += arr_obj.join(', ') +' }';
       } else {
@@ -75,19 +76,35 @@ if (typeof console === 'undefined') {
       }
 
       return result;
-    };
-
-    for (var i=0; i<arguments.length; i++) {
-      post( source_of(arguments[i]) );
     }
 
-  };
+    var result = [];
+    for (var i=0; i<arguments.length; i++) {
+      result.push( source_of_one_arg(arguments[i]) );
+    }
+    return result.join(', ');
+
+  }
+
+
+  var browser_suck_at_logging = /*@cc_on 1 || @*/ window.opera;
+
+  var log_methods = ['log', 'info', 'warn', 'error', 'debug', 'dir', 'dirxml'];
+
+  for (var i=0; i<log_methods.length; i++) {
+    var _log = console[log_methods[i]];
+    if (browser_suck_at_logging || !console[log_methods[i]]) {
+      console[log_methods[i]] = function () {
+        (_log || post)( source_of.apply(this, arguments) );
+      };
+    }
+  }
 
 
   /**
    * Simplified version of http://eriwen.com/javascript/js-stack-trace/
    */
-  console.trace = function trace() {
+  console.trace = console.trace || function trace() {
     try {
       i.dont.exist++;
     } catch(e) {
@@ -103,7 +120,7 @@ if (typeof console === 'undefined') {
    * @param {Boolean} is_ok
    * @param {String} message optional
    */
-  console.assert = function assert (is_ok, message) {
+  console.assert = console.assert || function assert (is_ok, message) {
     if (!is_ok) post( 'ASSERT FAIL: '+ message );
   };
 
@@ -111,15 +128,16 @@ if (typeof console === 'undefined') {
   /**
    * @param {String} name optional
    */
-  console.group = console.groupCollapsed = function group (name) {
+  console.group = console.group || function group (name) {
     post('\n-------------------- '+ name +' --------------------');
   };
 
+  console.groupCollapsed = console.groupCollapsed = console.group;
 
   /**
    * Print 3 line breaks
    */
-  console.groupEnd = function groupEnd () {
+  console.groupEnd = console.groupEnd || function groupEnd () {
     post('\n\n\n');
   };
 
@@ -127,7 +145,7 @@ if (typeof console === 'undefined') {
   /**
    * @param {String} title optional
    */
-  console.count = function count (title) {
+  console.count = console.count || function count (title) {
     var title = title || '';
     arguments.callee.counters = arguments.callee.counters || {};
     if (arguments.callee.counters[title]) {
@@ -142,10 +160,10 @@ if (typeof console === 'undefined') {
   /**
    * @param {String} title optional
    */
-  console.profile = function profile (title) {
+  console.profile = console.profile || function profile (title) {
     return 'Not implemented';
   };
-  console.profileEnd = function profileEnd () {
+  console.profileEnd = console.profileEnd || function profileEnd () {
     return 'Not implemented';
   };
 
@@ -155,7 +173,7 @@ if (typeof console === 'undefined') {
   /**
    * @param {String} name such as "my damn slow parser"
    */
-  console.time = function time (name) {
+  console.time = console.time || function time (name) {
     var start = (new Date).getTime();
     console._timers[name] = {'start': start};
   };
@@ -163,11 +181,10 @@ if (typeof console === 'undefined') {
   /**
    * @param {String} name such as "my damn slow parser"
    */
-  console.timeEnd = function timeEnd (name) {
+  console.timeEnd = console.timeEnd || function timeEnd (name) {
     var end = (new Date).getTime();
     console.info(name +': '+ (end - console._timers[name].start) +'ms');
     console._timers[name].end = end;
   };
 
-
-}
+})();
